45e27a669c3adf492f6191cad22f36ebc7c23884
Karim Marabet <newrabgo@gmail.com>
2021-11-26 15:02:02 +0300

    Get json data from Rails API

--- src/PhotoList/Photo.js
+++ src/PhotoList/Photo.js
@@ -5,7 +5,7 @@ export default function Photo(props) {
 		<div>
 			<p>{props.photo.name}</p>
 			{/*<p>{props.photo.image}</p>*/}
-			<img src={`http://localhost:3001/${props.photo.image}`} />
+			<img src={`http://localhost:3001/${props.photo.image}`} width="200px" height = '200px' />
 		</div>
 	)
 }
--- src/PhotoList/PhotoForm.js
+++ src/PhotoList/PhotoForm.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { DirectUpload } from 'activestorage';
 
 class PhotoForm extends Component {
 	constructor(props) {
@@ -7,6 +8,7 @@ class PhotoForm extends Component {
 		this.state = {
 			api_url: props.api_url,
 			name: "",
+			image: {}
 		}
 
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,20 +20,55 @@ class PhotoForm extends Component {
 		this.formSubmit(event.target);
 	}
 
-	async formSubmit(formData) {
+	formSubmit(formData) {
+		
 		const data = new FormData(formData);
-		await fetch(this.state.api_url, {
+
+		fetch(this.state.api_url, {
 			method: "POST",
 			mode: "cors",
 			body: data
-		}).then(response => response.json())
-		.then(response => this.props.updatePhotoList(response))
+		})
+		.then(response => response.json())
+		// .then(data => console.log(data))
+		.then(data => this.uploadFile(this.state.image, data))
+		// .then(response => this.props.updatePhotoList(response))
+	}
+
+	uploadFile = (file, data) => {
+		const upload = new DirectUpload(file, 'http://127.0.0.1:3001/rails/active_storage/direct_uploads')
+		
+		upload.create((error, blob) => {
+			if (error) {
+				console.log(error)
+			} else {
+				fetch(`${this.state.api_url}/${data.id}`, {
+					method: "PUT",
+					headers: {
+						'Content-Type': 'application/json',
+						'Accept': 'application/json'
+					},
+					mode: "cors",
+					body: JSON.stringify({image: blob.signed_in})
+				})
+				.then(response => response.json())
+				.then(data => this.props.updatePhotoList(data))
+			}
+		})
 	}
 
 	handleNameChange(event) {
-		this.setState({
-			photo: event.target.value
-		})
+		if (event.target.name === 'image') {
+			console.log(event.target.files[0])
+			this.setState({
+				[event.target.name]: event.target.files[0]
+			})
+		} else {
+			console.log(event.target.value)
+			this.setState({
+				[event.target.name]: event.target.value
+			})
+		}
 	}
 
 	render() {
@@ -41,14 +78,24 @@ class PhotoForm extends Component {
 				onSubmit={this.handleSubmit}
 				id="photo_form"
 				autoComplete="off">
-					<label>
-						Name:
+					<label><br />
+						Name: <br />
 				    	<input 
 				    	id="name_input"
 				    	type="text" 
-				    	name="photo[name]"
+				    	name="name"
 				    	onChange={this.handleNameChange }/>
 					</label>
+
+					<label><br /><br />
+						Image upload: <br />
+				    	<input 
+				    	id="image_input"
+				    	type="file" 
+				    	name="image"
+				    	onChange={this.handleNameChange }/>
+					</label><br /><br />
+
 				 	<input type="submit" value="Submit" />
 				</form>
 			</div>
