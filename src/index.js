aa7fcf0e8ab3babaa04379ef8b47efb2447e7c77
Karim Marabet <newrabgo@gmail.com>
2021-12-09 11:58:25 +0300

    Install redix with toolkit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import store from './store'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +10,11 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/authSlice.js
@@ -0,0 +1,11 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+  name: 'users',
+  initialState: {
+    currentUser: {}
+  },
+  reducers: {
+    
+  }
+})
